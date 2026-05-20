4995ff3fe862cbfd2fe5d6c8ffb432b90d231645
bilawalalimanthar-bit <bilawalalimanthar@gmail.com>
2026-05-20 18:59:15 +0500

    Remove table reservation form submission handler

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-document.getElementById("reservationForm")
-.addEventListener("submit", function(e){
 
-
-e.preventDefault();
-
-
-alert("Your table reservation has been submitted successfully!");
-
-
-});
